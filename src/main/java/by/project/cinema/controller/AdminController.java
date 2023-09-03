4e4621fa68de2df41159f7ffedcbbe1c0ae23e43
mchernova <mchernova3706@gmail.com>
2023-09-04 00:50:17 +0300

    cinema project: menu is done

--- src/main/java/by/project/cinema/controller/AdminController.java
+++ src/main/java/by/project/cinema/controller/AdminController.java
@@ -3,6 +3,8 @@ package by.project.cinema.controller;
 import by.project.cinema.model.Role;
 import by.project.cinema.model.User;
 
+import java.util.InputMismatchException;
+
 import static by.project.cinema.util.Constants.*;
 import static by.project.cinema.util.Util.*;
 
@@ -22,68 +24,87 @@ public class AdminController {
                 case "1" -> userService.createUser();                                                                   //create user
                 case "2" -> {
                     System.out.println(ENTER_USER_ID);
-                    int id = sc.nextInt();
-                    sc.nextLine();
-
-                    if (userService.isExistUser(id)) {
-                        User user = userService.getUserById(id);
-
-                        System.out.println(USER_NEW_USERNAME);
-                        String username = sc.nextLine();
-                        if (!username.isEmpty()) {
-                            while (userService.isExistUserByUsername(username)) {
-                                System.out.println(USER_EXISTS + TRY_AGAIN);
-                                username = sc.nextLine();
-                            }
-                            user.setUsername(username);
-                        }
-
-                        System.out.println(USER_NEW_PASSWORD);
-                        String password = sc.nextLine();
-                        if (!password.isEmpty()) {
-                            while (!userService.isPasswordValid(password)) {
-                                System.out.println(PASSWORD_NOT_VALID + PASSWORD_RULE + TRY_AGAIN);
-                                password = sc.nextLine();
-                            }
-                            user.setPassword(password);
-                        }
-
-                        System.out.println(USER_NEW_EMAIL);
-                        String email = sc.nextLine();
-                        if (!email.isEmpty()) {
-                            while (!userService.isEmailValid(email)) {
-                                System.out.println(EMAIL_NOT_VALID + TRY_AGAIN);
-                                email = sc.nextLine();
-                            }
-                            user.setEmail(email);
-                        }
-
-                        System.out.println(USER_NEW_ROLE);
-                        int roleId = sc.nextInt();
+//                    boolean validUserId = false;
+                    try {
+                        int id = sc.nextInt();
                         sc.nextLine();
-                        if (roleId == 1 || roleId == 2 || roleId == 3) {
-                            user.setRole(Role.getByOrdinal(--roleId));
-                        }
-                        if (userService.updateUser(user)) {
-                            System.out.println(USER_UPDATED);
-                        } else {
-                            System.out.println(NOT_SUCCESSFUL);
+
+                        if (userService.isExistUser(id)) {
+//                            validUserId = true;
+                            User user = userService.getUserById(id);
+
+                            System.out.println(USER_NEW_USERNAME);
+                            String username = sc.nextLine();
+                            if (!username.isEmpty()) {
+                                while (userService.isExistUserByUsername(username)) {
+                                    System.out.println(USER_EXISTS + TRY_AGAIN);
+                                    username = sc.nextLine();
+                                }
+                                user.setUsername(username);
+                            }
+
+                            System.out.println(USER_NEW_PASSWORD);
+                            String password = sc.nextLine();
+                            if (!password.isEmpty()) {
+                                while (!userService.isPasswordValid(password)) {
+                                    System.out.println(PASSWORD_NOT_VALID + PASSWORD_RULE + TRY_AGAIN);
+                                    password = sc.nextLine();
+                                }
+                                user.setPassword(password);
+                            }
+
+                            System.out.println(USER_NEW_EMAIL);
+                            String email = sc.nextLine();
+                            if (!email.isEmpty()) {
+                                while (!userService.isEmailValid(email)) {
+                                    System.out.println(EMAIL_NOT_VALID + TRY_AGAIN);
+                                    email = sc.nextLine();
+                                }
+                                user.setEmail(email);
+                            }
+
+                            System.out.println(USER_NEW_ROLE);
+                            int roleId = sc.nextInt();
+                            sc.nextLine();
+                            if (roleId == 1 || roleId == 2 || roleId == 3) {
+                                user.setRole(Role.getByOrdinal(--roleId));
+                            }
+                            if (userService.updateUser(user)) {
+                                System.out.println(USER_UPDATED);
+                            } else {
+                                System.out.println(NOT_SUCCESSFUL);
+                            }
+
+                        }else{
+                            System.out.println("Unknown user id");
                         }
 
+
+
+
+                    }catch (InputMismatchException e){
+                        System.out.println("Unknown user id");
+                        sc.nextLine();
                     }
                 }                                                                                       //update user
                 case "3" -> {
                     System.out.println(ENTER_USER_ID);
-                    int id = sc.nextInt();
-                    sc.nextLine();
-                    if (userService.isExistUser(id)) {
-                        if (userService.delete(id)) {
-                            System.out.println(USER_DELETED);
+                    try {
+                        int id = sc.nextInt();
+                        sc.nextLine();
+                        if (userService.isExistUser(id)) {
+                            if (userService.delete(id)) {
+                                System.out.println(USER_DELETED);
+                            }
+                        } else {
+                            System.out.println(USER_NOT_FOUND);
                         }
-                    } else {
+                    } catch (InputMismatchException e) {
                         System.out.println(USER_NOT_FOUND);
+                        sc.nextLine();
                     }
-                }                                                                                       //delete user
+                }
+                //delete user
                 case "4" -> userService.getUsers();                                                                     //get all users
                 case "0" -> MainController.mainMenu();
                 default -> System.out.println(SOMETHING_WRONG);
